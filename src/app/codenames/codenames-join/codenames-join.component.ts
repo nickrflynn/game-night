9aff3bcf5dee0f69cecbbf95d5c2ff663846c7d7
Nick Flynn <nickrflynn@gmail.com>
2018-10-14 20:42:09 -0600

    Fix join game button disable attribute

--- src/app/codenames/codenames-join/codenames-join.component.ts
+++ src/app/codenames/codenames-join/codenames-join.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./codenames-join.component.scss']
 })
 export class CodenamesJoinComponent implements OnInit {
-    code: string = 'test';
-
     constructor() {}
 
     ngOnInit() {}
